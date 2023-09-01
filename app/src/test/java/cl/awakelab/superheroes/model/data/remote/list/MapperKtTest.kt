bf7341d20f046671f19c0985a6bba98822631a1e
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-09-01 03:15:36 -0400

    Se crea test unitario.

--- app/src/test/java/cl/awakelab/superheroes/model/data/remote/list/MapperKtTest.kt
+++ app/src/test/java/cl/awakelab/superheroes/model/data/remote/list/MapperKtTest.kt
@@ -1,14 +1,27 @@
 package cl.awakelab.superheroes.model.data.remote.list
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MapperKtTest {
 
     @Test
     fun transformToEntity() {
+        //Given
+        val hero = Hero(1, 1935, "www.prueba.cl", "Chapulin Colorado",
+            "Chapultepec", "Encogerse")
+
+        //When
+        val resultTransform = hero.transformToEntity()
+
+        //Then
+        assertEquals(hero.id, resultTransform.id)
+        assertEquals(hero.anioCreacion, resultTransform.anioCreacion)
+        assertEquals(hero.imagenLink, resultTransform.imagenLink)
+        assertEquals(hero.nombre, resultTransform.nombre)
+        assertEquals(hero.origen, resultTransform.origen)
+        assertEquals(hero.poder, resultTransform.poder)
     }
 
-    @Test
-    fun transformToDetailEntity() {
-    }
+
 }
